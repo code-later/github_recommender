dbb79c23190db519b0cecc49b450e417423473d3
Dirk Breuer <dirk.breuer@gmail.com>
2014-11-20 10:56:48 +0100

    Moved import of GitHub data into dedicated class

--- app/models/github_importer.rb
+++ app/models/github_importer.rb
@@ -8,9 +8,24 @@ class GithubImporter
                                   auto_paginate: true)
   end
 
-  def import_user(user)
+  def import_followings
+    client.following.each do |following|
+      u = UsersCollection.find_or_create_by_attributes(following)
+      import_repositories(u)
+    end
   end
 
-  def import_repositories(user)
+  def import_repositories(user = @user)
+    user.repositories = client.repos(user.login).map do |repo|
+      Repository.new(name: repo.name,
+                     full_name: repo.full_name,
+                     url: repo.url,
+                     created: repo.created_at,
+                     language: repo.language,
+                     watchers: repo.watchers,
+                     stargazers: repo.stargazers_count,
+                     forks: repo.forks)
+    end
+    UsersCollection.save user
   end
 end
